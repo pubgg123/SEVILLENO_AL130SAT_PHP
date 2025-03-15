1d34a0ca6a92f9fad0b3d4158e14250c702d86eb
pubgg123 <alcrisevilleno@gmail.com>
2025-03-15 18:24:22 +0800

    0315_Video20_starter-kits-breeze-and-middleware_lab

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+
