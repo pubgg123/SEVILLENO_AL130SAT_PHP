b3e73e8449b2b3a2d5d94187d3e94481a2ea192a
pubgg123 <alcrisevilleno@gmail.com>
2025-03-15 20:08:37 +0800

    0315_Video38_register-a-new-user_lec

--- controllers/contact.php
+++ controllers/contact.php
@@ -1,7 +1,5 @@
 <?php
 
-$_SESSION['last'] = 'Sevilleno';
-
 view("contact.view.php", [
     'heading' => 'Contact Us',
 ]);
--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$_SESSION['name'] = 'Alcris';
-
 view("index.view.php", [
     'heading' => 'Home',
 ]);
